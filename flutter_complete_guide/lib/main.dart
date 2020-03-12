30f230475a754137237acc010835a1cc0fad3018
Sergio Medina <smedinac8185@universidadean.edu.co>
2020-03-12 18:53:29 -0500

    irst Styling & Layouting Steps

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -33,8 +33,15 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context){
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal',
+      {'questionText': 'What\'s your favorite color?', 'answer': [
+        'Black', 'Red', 'Green', 'White'],
+      },
+      {'questionText': 'What\'s your favorite animal',
+       'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {'questionText': 'Who\'s your favorite instructor?',
+       'answer': ['Max', 'Max', 'Max', 'Max'],
+      },    
     ];
 
     return MaterialApp(
