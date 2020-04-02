c74074c6bec5bd7f439280aedf3245a314a75177
Sergio Medina <smedinac8185@universidadean.edu.co>
2020-04-02 13:26:21 -0500

    Calculating a Total Score/ Getters & else-if/ Resetting the Quiz

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -21,20 +21,45 @@ class MyApp extends StatefulWidget  {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
       {'questionText': 'What\'s your favorite color?', 'answer': [
-        'Black', 'Red', 'Green', 'White'],
+        {'text': 'Black', 'score': 10}, 
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3}, 
+        {'text': 'White', 'score': 1},
+       ],
       },
       {'questionText': 'What\'s your favorite animal',
-       'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+       'answer': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake',  'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+       ],
       },
       {'questionText': 'Who\'s your favorite instructor?',
-       'answer': ['Max', 'Max', 'Max', 'Texas'],
+       'answer': [
+       {'text': 'Max', 'score': 1}, 
+       {'text':  'Max', 'score': 1},
+       {'text':  'Max', 'score': 1},
+       {'text':  'Texa', 'score': 1},
+       ],
       },    
     ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+      setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
     
+  }
+
+  void _answerQuestion(int score) {
+    
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -67,7 +92,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex, 
               questions: _questions,
               )  
-        : Result()
+        : Result(_totalScore, _resetQuiz),
       ),
     );
   }
